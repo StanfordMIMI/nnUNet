591560dd87d4f7a42d6c82e22efef761ec528abf
Louis Blankemeier <louis.blankemeier@stanford.edu>
2024-04-17 18:37:47 -0700

    Update generic_UNet.py

--- nnunet/network_architecture/generic_UNet.py
+++ nnunet/network_architecture/generic_UNet.py
@@ -360,12 +360,15 @@ class Generic_UNet(SegmentationNetwork):
 
         self.upscale_logits_ops = []
         cum_upsample = np.cumprod(np.vstack(pool_op_kernel_sizes), axis=0)[::-1]
+
+        def identity(x):
+            return x
         for usl in range(num_pool - 1):
             if self.upscale_logits:
                 self.upscale_logits_ops.append(Upsample(scale_factor=tuple([int(i) for i in cum_upsample[usl + 1]]),
                                                         mode=upsample_mode))
             else:
-                self.upscale_logits_ops.append(lambda x: x)
+                self.upscale_logits_ops.append(identity)
 
         if not dropout_in_localization:
             self.dropout_op_kwargs['p'] = old_dropout_p
